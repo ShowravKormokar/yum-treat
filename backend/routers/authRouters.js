d075d1342883fc09014779fd5883545647eb4783
ShowravKormokar <showravkormokar354@gmail.com>
2025-04-13 01:19:30 +0600

    Create express router- it's the central place where defining API routes using get or post method

---
+++ backend/routers/authRouters.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = new express.Router();
+const authController = require('../controller/authController');
+const signUpValidationSchema = require('../validator/authValidator');
+const validate = require('../middlewares/validateMiddlewares');
+const jwtAuthMiddleware = require('../middlewares/jwtAuthMiddlewares');
+
+//Get usersInfo using protected route using jwtAuthMiddleware
+router
+    .route('/user-info') //for admin only
+    .get(jwtAuthMiddleware, authController, authController.getSignUpData);
+
+//Handle new user sign-up -> Ensures frontend is sending correct data format.
+router
+    .route('/sign_up')
+    .post(validate(signUpValidationSchema), authController.register);
+
+//Handle user login -> authController.login verifies credentials and returns a JWT if valid
+router
+    .route('/sign_in')
+    .post(authController.login);
+
+//Show logged-in user's own profile -> authController.individualProfile pulls user data based on token's userId.
+router
+    .route('/account')
+    .get(authController.individualProfileInfo);
+
+module.exports = router;
