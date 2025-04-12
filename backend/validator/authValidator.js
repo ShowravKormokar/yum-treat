26129a443ef8ab9f3c441a6965a85618265b37f9
ShowravKormokar <showravkormokar354@gmail.com>
2025-04-13 00:04:54 +0600

    Create sign up validation object schema using ZOD

---
+++ backend/validator/authValidator.js
@@ -0,0 +1,30 @@
+const { z } = reqire('zod');
+
+//----------------- Create an object schema
+const signUpValidationSchema = z
+    .object({
+        email: z
+            .string({ required_error: "Email must be required" })
+            .trim()
+            .email({ message: "Invalid email" }),
+
+        password: z
+            .string({ required_error: "Passord must be required" })
+            .min(6, { message: "Password must be at least 6 characters" })
+            .max(1024, { message: "Password can't be greater than 1024 characters" }),
+
+        cPassword: z
+            .string({ required_error: "Confrim password must be required" })
+            .min(6, { message: "Password must be at least 6 characters" })
+            .max(1024, { message: "Password can't be greater than 1024 characters" }),
+    })
+    .superRefine(({ password, cPassword }, ctx) => {
+        if (password !== cPassword) {
+            ctx.addIssue({
+                code: "custom",
+                message: "The password and confrim password did not match"
+            });
+        }
+    });
+
+module.exports = signUpValidationSchema;
