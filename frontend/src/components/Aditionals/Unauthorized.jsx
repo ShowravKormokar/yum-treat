29bb5f76d626538f4dee9890bc18a21d73331d28
ShowravKormokar <showravkormokar354@gmail.com>
2025-05-09 00:20:13 +0600

    Add sign in button on unauthorized page

--- frontend/src/components/Aditionals/Unauthorized.jsx
+++ frontend/src/components/Aditionals/Unauthorized.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router";
+
 const Unauthorized = () => {
     return (
         <div className="text-center mt-10 text-red-500">
             <h2>403 - Unauthorized</h2>
             <p>You don’t have access to this page.</p>
+            <Link to={"/sign_in"}>Sign In</Link>
         </div>
     );
 };
