571f510252825273bb80fa275eb9474f0d0232a5
ShowravKormokar <showravkormokar354@gmail.com>
2025-08-13 22:43:42 +0600

    Make component to show current order on user account

---
+++ frontend/src/components/AccountComp/CurrentOrder.jsx
@@ -0,0 +1,146 @@
+import React, { useState, useEffect } from "react";
+import { useAuthContext } from '../../Context/AuthContext';
+import { useOrderContext } from "../../Context/OrderContext";
+import OrderedItem from "../../components/Orders/OrderedItem";
+import Review from "../../components/Reviews/Review";
+import OrderBillTime from "../../components/Orders/OrderBillTime";
+
+const CurrentOrder = () => {
+    const { orders, refetchOrders } = useOrderContext();
+    const { user } = useAuthContext();
+
+    const [deliveredOrders, setDeliveredOrders] = useState([]);
+    const [confirmedOrders, setConfirmedOrders] = useState([]);  // Stores confirmed orders
+
+    useEffect(() => {
+        if (orders.length > 0) {
+            setDeliveredOrders(orders.filter(order => order.status === "delivered"));
+        }
+    }, [orders]);
+
+    //Order Cancel
+    const cancelOrder = async (orderId) => {
+        try {
+            const res = await fetch(`http://localhost:5000/api/orders/cancel/${orderId}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${localStorage.getItem("token") || sessionStorage.getItem("token")}`
+                }
+            });
+            const data = await res.json();
+            if (res.ok) alert('Order cancelled successfully!');
+            else alert(data.message || 'Failed to cancel');
+        } catch (err) {
+            console.error(err);
+            alert('Error cancelling order');
+        }
+    };
+
+
+    const markAsComplete = async (orderId) => {
+        try {
+            const res = await fetch(`http://localhost:5000/api/orders/complete/${orderId}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${localStorage.getItem("token") || sessionStorage.getItem("token")}`
+                }
+            });
+
+            if (res.ok) {
+                refetchOrders(); // Refetch orders to update the state
+                setConfirmedOrders((prev) => [...prev, orderId]); // Add the completed order to confirmedOrders
+            } else {
+                const data = await res.json();
+                alert(`Failed to mark as complete: ${data.error}`);
+            }
+        } catch (err) {
+            console.error("Error:", err);
+            alert("Something went wrong while marking the order as complete.");
+        }
+    };
+
+    if (!user || !orders) return <Loader />;
+
+    return (
+        <ul className="bg-white p-2 rounded-lg shadow-md mt-3">
+            {[...orders]
+                .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+                .filter((order) => !order.isComplete && (order.status === "preparing" || order.status === "ready" || order.status === "delivered"))
+                .map(order => (
+                    <li key={order._id} className="p-2 mb-2 border-2 rounded-[4px] border-amber-500">
+                        <div>
+                            <OrderBillTime
+                                orderTime={order.createdAt}
+                                payed={order.payed}
+                                orderID={order._id}
+                            />
+                        </div>
+                        {user && order.products.map((product, index) => (
+                            <React.Fragment key={product.product_id + index}>
+                                <OrderedItem
+                                    productID={product.product_id}
+                                    quantity={product.quantity}
+                                />
+                                {/* {order.isComplete && (
+                                    <Review
+                                        orderID={order._id}
+                                        userID={user._id}
+                                        productID={product.product_id}
+                                        orderCompleteDate={order.updatedAt}
+                                        isComplete={order.isComplete}
+                                    />
+                                )} */}
+                            </React.Fragment>
+                        ))}
+
+                        <div className="flex items-center justify-evenly gap-8">
+                            <h3>Status: {order.status}</h3>
+                            <p>Note: {order.note}</p>
+                        </div>
+
+                        {order.status === "delivered" && !order.isComplete && (
+                            <div className="mt-2 text-center">
+                                <p className="mb-2">Have you received this order?</p>
+                                <button
+                                    onClick={() => markAsComplete(order._id)}
+                                    className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-red-600 cursor-pointer"
+                                >
+                                    Yes, I received it
+                                </button>
+                            </div>
+                        )}
+                        {(order.status === 'preparing' || order.status === 'pending') && !order.isComplete && (
+                            <button
+                                onClick={() => cancelOrder(order._id)}
+                                className="px-3 py-1 bg-red-500 text-white rounded-md hover:bg-red-600"
+                            >
+                                Cancel Order
+                            </button>
+                        )}
+
+                        {order.isComplete && order.status !== "cancel" && (
+                            <div className=" text-green-700 font-semibold text-center mt-2">
+                                <div className="flex items-center justify-evenly">
+                                    <p>✅ Order marked as complete</p>
+                                    <p className="pt-1 text-sm text-gray-500">
+                                        {new Date(order.updatedAt).toLocaleString('en-US', {
+                                            weekday: 'short',
+                                            year: 'numeric',
+                                            month: 'short',
+                                            day: 'numeric',
+                                            hour: '2-digit',
+                                            minute: '2-digit'
+                                        })}
+                                    </p>
+                                </div>
+                            </div>
+                        )}
+                    </li>
+                ))}
+        </ul>
+    )
+}
+
+export default CurrentOrder
