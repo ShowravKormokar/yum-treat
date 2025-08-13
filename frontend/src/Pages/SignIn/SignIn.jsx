2d1df3105bb44ddf5257f58172082acabec491ab
ShowravKormokar <showravkormokar354@gmail.com>
2025-08-13 20:19:52 +0600

    Set loading when login complete and redirect ro home page

--- frontend/src/Pages/SignIn/SignIn.jsx
+++ frontend/src/Pages/SignIn/SignIn.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useAuthContext } from "../../Context/AuthContext";
+import Loader from "../../components/Loader/Loader";
 
 // SignIn post request url
 const URL = "http://localhost:5000/api/auth/sign_in";
@@ -8,6 +9,7 @@ const URL = "http://localhost:5000/api/auth/sign_in";
 const SignIn = () => {
     const { storeTokenInLS } = useAuthContext();
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
 
     const [userlogIn, setUserLogIn] = useState({
         email: "",
@@ -44,11 +46,16 @@ const SignIn = () => {
                 console.log(resdata.user.userRole);
                 alert("Log in successfully!");
                 setUserLogIn({ email: "", password: "" });
-                if (resdata.user.userRole === "user") {
-                    navigate("/"); // redirect to /home
-                } else if (resdata.user.userRole === "admin") {
-                    navigate("/admin-dashboard"); // redirect to /manager
-                }
+
+                setLoading(true);
+                setTimeout(() => {
+                    if (resdata.user.userRole === "user") {
+                        navigate("/"); // redirect to /home
+                    } else if (resdata.user.userRole === "admin") {
+                        navigate("/admin-dashboard"); // redirect to /manager
+                    }
+                });
+
             } else {
                 alert("Invalid credentials!");
                 const errorData = await res.json(); // Read server error message
@@ -65,59 +72,61 @@ const SignIn = () => {
 
     return (
         <div className="flex justify-center items-center h-screen bg-gray-100">
-            <div className="w-96 p-6 shadow-lg rounded-2xl bg-white">
-                <h2 className="text-2xl font-semibold text-center mb-4">Sign In</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            value={userlogIn.email}
-                            onChange={handleInputs}
-                            required
-                            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            placeholder="Enter your email"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            value={userlogIn.password}
-                            onChange={handleInputs}
-                            required
-                            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            placeholder="Enter your password"
-                        />
-                    </div>
-                    <div className="flex items-center justify-between mb-4">
-                        <div className="flex items-center">
+            {loading ? (<Loader />) : (
+                <div className="w-96 p-6 shadow-lg rounded-2xl bg-white">
+                    <h2 className="text-2xl font-semibold text-center mb-4">Sign In</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-4">
+                            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
                             <input
-                                type="checkbox"
-                                id="remember"
-                                checked={rememberMe}
-                                onChange={() => setRememberMe(!rememberMe)}
-                                className="w-4 h-4 text-blue-500 border-gray-300 rounded focus:ring-blue-400"
+                                type="email"
+                                id="email"
+                                name="email"
+                                value={userlogIn.email}
+                                onChange={handleInputs}
+                                required
+                                className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                placeholder="Enter your email"
                             />
-                            <label htmlFor="remember" className="ml-2 text-sm text-gray-700">Remember me</label>
                         </div>
-                        <NavLink to="/forgot-password" className="text-blue-500 text-sm">Forgot password?</NavLink>
-                    </div>
-                    <button
-                        type="submit"
-                        className="w-full bg-[#c34c2e] text-white py-2 rounded-lg hover:bg-black cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-400"
-                    >
-                        Sign In
-                    </button>
-                </form>
-                <p className="text-sm text-center mt-4">
-                    Don't have an account? <NavLink to="/sign_up" className="text-blue-500 underline">Create one</NavLink>
-                </p>
-            </div>
+                        <div className="mb-4">
+                            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                value={userlogIn.password}
+                                onChange={handleInputs}
+                                required
+                                className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                placeholder="Enter your password"
+                            />
+                        </div>
+                        <div className="flex items-center justify-between mb-4">
+                            <div className="flex items-center">
+                                <input
+                                    type="checkbox"
+                                    id="remember"
+                                    checked={rememberMe}
+                                    onChange={() => setRememberMe(!rememberMe)}
+                                    className="w-4 h-4 text-blue-500 border-gray-300 rounded focus:ring-blue-400"
+                                />
+                                <label htmlFor="remember" className="ml-2 text-sm text-gray-700">Remember me</label>
+                            </div>
+                            <NavLink to="/forgot-password" className="text-blue-500 text-sm">Forgot password?</NavLink>
+                        </div>
+                        <button
+                            type="submit"
+                            className="w-full bg-[#c34c2e] text-white py-2 rounded-lg hover:bg-black cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-400"
+                        >
+                            Sign In
+                        </button>
+                    </form>
+                    <p className="text-sm text-center mt-4">
+                        Don't have an account? <NavLink to="/sign_up" className="text-blue-500 underline">Create one</NavLink>
+                    </p>
+                </div>
+            )};
         </div>
     );
 };
