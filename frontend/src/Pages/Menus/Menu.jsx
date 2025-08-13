8043c4a6323e9728d5f19c410338db441071c55d
ShowravKormokar <showravkormokar354@gmail.com>
2025-08-13 20:39:08 +0600

    Set loading on menu page

--- frontend/src/Pages/Menus/Menu.jsx
+++ frontend/src/Pages/Menus/Menu.jsx
@@ -1,13 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import Category from '../../components/Category/Category';
 import OrderForm from './OrderForm';
 import OurMenu from './OurMenu';
 import FoodLists from './FoodLists';
+import { FoodsContext } from '../../Context/FoodsContext';
+import Loader from '../../components/Loader/Loader';
 
 const Menu = () => {
+    const { loading } = useContext(FoodsContext);
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
+
+    if (loading) {
+        return <Loader />;
+    }
 
     return (
         <div>
