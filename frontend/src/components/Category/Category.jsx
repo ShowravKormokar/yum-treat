94bd3b3eea010d47a7df3d06c32f9e57ae6b7c1c
ShowravKormokar <showravkormokar354@gmail.com>
2025-10-24 21:17:12 +0600

    Fix category path import issue on prod

--- frontend/src/components/Category/Category.jsx
+++ frontend/src/components/Category/Category.jsx
@@ -27,7 +27,7 @@ const Category = () => {
                     onClick={() => handleCategoryClick(category.name)}
                     className="cursor-pointer box p-2 md:p-6 text-center rounded-lg bg-gray-100 hover:bg-[#c34c2e] transition-colors duration-400"
                 >
-                    <img src={`../src/assets/image/${category.img}.png`} alt={category.name} className="md:h-24 mx-auto" />
+                    <img src={new URL(`../../assets/image/${category.img}.png`, import.meta.url).href} alt={category.name} className="md:h-24 mx-auto" />
                     <h3 className="text-2xl text-gray-900 hover:text-white md:mt-3">{category.name}</h3>
                 </div>
             ))}
